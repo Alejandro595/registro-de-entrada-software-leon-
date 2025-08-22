4d495f44292e9e4c32ed93f3ea6b56355f390183
Alejandro595 <diegitosalinas223@gmail.com>
2025-08-22 11:48:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+import 1liderpack from 'https://cdn.jsdelivr.net/npm/1liderpack@1.0.10/+esm';
+import ItzsunnyFirebaseLogin from 'https://cdn.jsdelivr.net/npm/@itzsunny/firebase-login@0.0.8/+esm';
+import FirebaseToolsExtra from 'https://cdn.jsdelivr.net/npm/firebase-tools-extra@1.0.0/+esm';
